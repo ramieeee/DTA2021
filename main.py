182ca188e789e2da27230c4c66af296fa4570dab
ramieeee <rambotty@gmail.com>
2021-10-18 23:26:04 +0900

    add find code to find p tag

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import bs4
 
-webpage=urlopen('http://www.dentalarirang.com/news/articleView.html?idxno=33129')
-source = BeautifulSoup(webpage, 'html.parser', from_encoding='utf-8')
+# article from 33000 - 33129
+for i in range(33000, 33130):
+    webpage=urlopen('http://www.dentalarirang.com/news/articleView.html?idxno=33129')
+    source = BeautifulSoup(webpage, 'html.parser', from_encoding='utf-8')
+    source.findAll('p')
